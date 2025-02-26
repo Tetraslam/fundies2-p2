13c276075a335c8977faf6cc23f1911cdc653812
lyannne <xu.ly@northeastern.edu>
2025-02-25 21:32:57 -0500

    added constructors

--- hw6.3/src/huffman.java
+++ hw6.3/src/huffman.java
@@ -1,2 +1,44 @@
 import tester.*;
+import java.util.ArrayList;
 
+class Huffman {
+    ArrayList<String> alphabet;
+    ArrayList<Integer> frequencies;
+
+    Huffman(ArrayList<String> alphabet, ArrayList<Integer> frequencies) {
+        if (alphabet.size() != frequencies.size()) {
+            throw new IllegalArgumentException("Alphabet and frequencies must be the same size");
+        }
+
+        else if (alphabet.size() < 2) {
+            throw new IllegalArgumentException("Alphabet and frequencies must not be empty");
+        }
+        
+        this.alphabet = alphabet;
+        this.frequencies = frequencies;
+    }
+}
+
+interface ITree {
+}
+
+class Leaf implements ITree {
+    String value;
+    int frequency;
+
+    Leaf(String value, int frequency) {
+        this.value = value;
+        this.frequency = frequency;
+    }
+}
+
+class Node implements ITree {
+    int totalFrequency;
+    ITree left;
+    ITree right;
+
+    Node(int totalFrequency, ITree left, ITree right) {
+        this.left = left;
+        this.right = right;
+    }
+}
